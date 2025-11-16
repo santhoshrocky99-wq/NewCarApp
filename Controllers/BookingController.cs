e6564da6e98382657561f9b4681acbe634c95979
Arun Kumar <arukmr1990@gmail.com>
2025-11-16 21:04:12 +0530

    Post method is fixed in Controller

--- Controllers/BookingController.cs
+++ Controllers/BookingController.cs
@@ -35,50 +35,10 @@ namespace CarCleanz.Controllers
         }
 
         [HttpPost]
-[IgnoreAntiforgeryToken]
 public IActionResult Create(Booking booking)
+{
+    return Content("POST RECEIVED OK");
+}
 
-        {
-            if (!ModelState.IsValid)
-            {
-                return View(booking);
-            }
-
-            try
-            {
-                var lastBooking = _context.Bookings
-                    .OrderByDescending(b => b.Id)
-                    .FirstOrDefault();
-
-                int nextNumber = 3000;
-
-                if (lastBooking != null && !string.IsNullOrEmpty(lastBooking.CustomBookingId))
-                {
-                    string numberPart = lastBooking.CustomBookingId.Replace("CCA", "");
-                    nextNumber = int.Parse(numberPart) + 1;
-                }
-
-                booking.CustomBookingId = $"CCA{nextNumber}";
-
-                booking.BookingDate = DateTime.SpecifyKind(booking.BookingDate, DateTimeKind.Utc);
-
-                switch ((booking.VehicleType ?? "").ToLower())
-                {
-                    case "hatchback": booking.Price = 499; break;
-                    case "sedan": booking.Price = 650; break;
-                    case "suv": booking.Price = 750; break;
-                    default: booking.Price = 0; break;
-                }
-
-                _context.Bookings.Add(booking);
-                _context.SaveChanges();
-
-                return RedirectToAction("Payment", new { id = booking.Id });
-            }
-            catch (Exception ex)
-            {
-                return Content("ERROR: " + ex.ToString());
-            }
-        }
     }
 }
